4b52ab49e59098132f5e31b6e184b80c44ae6bda
Tteocarre <perre.matteo@gmail.com>
2025-05-02 23:26:25 +0200

    g un peu modif le js, et c bon ca marche, je vais faire les icones de soleil et lune, je pense y feront environ 12x12

--- scripts/change-theme.js
+++ scripts/change-theme.js
@@ -1,10 +1,10 @@
 function initThemeSwitcher() {
     const button = document.getElementById('change-theme');
     button.addEventListener('click', function() {
-        document.body.classList.toggle('dark-theme');
+        document.body.classList.toggle('light_theme');
 
         // Changer le texte du bouton en fonction du mode
-        if (document.body.classList.contains('dark-theme')) {
+        if (document.body.classList.contains('light_theme')) {
             button.textContent = '';
         } else {
             button.textContent = '';
