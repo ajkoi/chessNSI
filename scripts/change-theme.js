ec52d7046151910444b9254d725f1742eed0b0f4
ajkoi <felix.yiou.alias.yeou@gmail.com>
2025-05-03 10:36:02 +0200

    adding an menu.html qui est load avec jquery

--- scripts/change-theme.js
+++ scripts/change-theme.js
@@ -3,7 +3,7 @@
         document.body.classList.toggle('light_theme');
         const my_img_now = my_img.getAttribute('src');
         if (my_img_now === 'images/menu/lune_css.png') {
-            my_img.setAttribute('src', 'images/menu/lune_css.png');
+            my_img.setAttribute('src', 'images/menu/sol_css.png');
         } else {
             my_img.setAttribute('src', 'images/menu/lune_css.png');
         }
