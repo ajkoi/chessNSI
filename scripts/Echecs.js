2b9f9ab1e61ecba79548a2ff67138bf4966ee1c9
ajkoi <felix.yiou.alias.yeou@gmail.com>
2025-05-04 12:51:17 +0200

    chess game

--- scripts/Echecs.js
+++ scripts/Echecs.js
@@ -14,15 +14,39 @@ let chessboard = [
 
 let player = 0
 let tour = 1
+let canPlayer1Roque = true
+let canPayer2Roque = true
 
 function jouerCoup (startTile, endTile) {
     /*
     * startTile: tuple(int, int)
     * endTile: tuple(int, int) */
-    chessboard[endTile[0]][endTile[1]] = chessboard[startTile[0]][startTile[1]]
-    chessboard[startTile[0]][startTile[1]] = ''
+    if (isPlayable(startTile, endTile)) {
+        chessboard[endTile[0]][endTile[1]] = chessboard[startTile[0]][startTile[1]]
+        chessboard[startTile[0]][startTile[1]] = ''
+    }
 }
 
 function isPlayable (startTile, endTile) {
     return true
+}
+
+function abs(numbers){
+    if (numbers>0) {
+        return numbers
+    }
+    else {
+        return -numbers
+    }
+
+}
+
+function isItaRoque(startTile, endTile) {
+    if (!( chessboard[startTile[0]][startTile[1]] === 'R' || chessboard[startTile[0]][startTile[1]] === 'r' )) {
+        return false
+    }
+    else if (abs(startTile[1]-startTile[0]) === 2) {
+        return true
+    }
+
 }
